8461ca5a5d56dce7c94ca13605507aa2700b46a6
Eric Kelly <heroiceric@gmail.com>
2014-09-28 14:22:13 -0400

    View and create Peels from from/in the DB

--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/lib/pq"
 	"html/template"
+	"log"
 	"net/http"
 )
+
+type Peel struct {
+	Id   int
+	Body string
+}
 
 func StartHTTPServer() {
 	fmt.Println("Server up and running at http://localhost:1337...")
@@ -13,9 +21,39 @@ func StartHTTPServer() {
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl, _ := template.ParseFiles("app/templates/index.html")
+		db, err := sql.Open("postgres", "dbname=kananas_development sslmode=disable")
+		if err != nil {
+			log.Fatal(err)
+		}
 
-		tmpl.Execute(w, nil)
+		peels := []Peel{}
+
+		rows, err := db.Query("SELECT * FROM peels")
+		for rows.Next() {
+			peel := Peel{}
+			if err := rows.Scan(&peel.Id, &peel.Body); err != nil {
+				log.Fatal(err)
+			}
+			peels = append(peels, peel)
+		}
+
+		db.Close()
+
+		tmpl, _ := template.ParseFiles("app/templates/index.html")
+		tmpl.Execute(w, peels)
+	})
+
+	http.HandleFunc("/peels", func(w http.ResponseWriter, r *http.Request) {
+		db, err := sql.Open("postgres", "dbname=kananas_development sslmode=disable")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		body := r.FormValue("body")
+		db.Exec("INSERT INTO peels (body) VALUES ($1)", body)
+
+		db.Close()
+		http.Redirect(w, r, "/", http.StatusFound)
 	})
 
 	StartHTTPServer()
---
+++ schema.sql
@@ -0,0 +1,4 @@
+CREATE TABLE peels (
+  id serial PRIMARY KEY,
+  body varchar(140) NOT NULL
+);
